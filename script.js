770fe19118a22ff6c070fb6dc970eac07022937c
izacoding <dsteele008@gmail.com>
2018-06-05 11:24:51 -0400

    LargeNumber Conditionals

---
+++ script.js
@@ -0,0 +1,16 @@
+// label the three varibles
+// store the three vairbles
+
+var num1 = prompt("what is your first number?");
+var num2 = prompt("what is your second number?");
+var num3 = prompt("what is your thrid number?");
+// Conditions all the vaule to show what is less than greather than or equal too
+if(num1>num2){
+console.log(num1 + "is the largest number"); 
+}else if(num2>num3){
+   console.log(num2 +"is the largest number");
+// } log out the largest number 
+else{
+     console.log(num3 + "is the largest number");
+   }
+ 
